8cae6571dbb93fbae2d3dc975345a961fcabb704
Leslie Rose <leslierose@Leslies-MBP.attlocal.net>
2018-07-07 22:40:13 -0500

    save functionality

--- public/app.js
+++ public/app.js
@@ -1,15 +1,9 @@
 $(document).on("click", ".scrapebutton", function () {
-    // Grab the articles as a json
-    $(".articles").empty();
-
-    $.getJSON("/getarticles", function (data) {
-        // For each one
-        for (var i = 0; i < data.length; i++) {
-            // Display the apropos information on the page
-            $(".articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "<button type='button' class='btn btn-primary saveButton' data-toggle='modal' data-target='#saveModal' data-id='" + data[i]._id + "'>Save</button>" + "</p>");
-        }
-    });
-
+    $.ajax({
+        method: "GET",
+        url: "/api/getarticles",
+    }).done(function(data) {
+    })
 })
 
 // Whenever someone clicks a save button
@@ -19,16 +13,10 @@ $(document).on("click", ".saveButton", function () {
 
     // Now make an ajax call for the Article
     $.ajax({
-        method: "GET",
-        url: "/savedarticles/" + savedNoteId,
+        method: "POST",
+        url: "/api/savedarticles/" + savedNoteId,
     })
-        // With that done, add the note information to the page
-        .then(function (data) {
-            console.log(data);
-            // The title of the article
-            $(".savedarticles").append("<h2>" + data.title + "</h2>");
-            // An input to enter a new title
-            $(".savedarticles").append("<h2>" + data.link + "</h2>");
-            data.saved = true;
-        });
-});
+    .done(function(data) {
+        window.location = "/"
+    })
+})
