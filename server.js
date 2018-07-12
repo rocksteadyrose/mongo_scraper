866c1beec2357a7a8a9d113149b595051bc47f75
Leslie Rose <leslierose@Leslies-MBP.attlocal.net>
2018-07-12 14:10:00 -0500

    updates for heroku

--- server.js
+++ server.js
@@ -24,7 +24,14 @@ app.engine("handlebars", exphbs({
 
 app.set("view engine", "handlebars");
 
-mongoose.connect("mongodb://localhost/mongo_scraper");
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongo_scraper";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
 
 app.listen(PORT, function () {
   console.log("Server listening on: http://localhost:" + PORT);
