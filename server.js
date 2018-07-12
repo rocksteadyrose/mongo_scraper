0d823969fe1422cc025faae1f7caa1d79f6b07e3
Leslie Rose <leslierose@Leslies-MBP.attlocal.net>
2018-07-12 14:45:27 -0500

    testing heroku

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var exphbs = require("express-handlebars");
 var path = require("path");
 var request = require('request');
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
@@ -31,6 +31,9 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongo_scraper"
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI, {
+  useMongoClient: true
+});
 
 
 app.listen(PORT, function () {
